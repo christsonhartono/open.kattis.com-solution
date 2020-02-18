84d538918be91203b41fcf8910b142f559e70539
Christson Hartono <42518369+christsonhartono@users.noreply.github.com>
2020-02-18 22:58:31 +0700

    Python

--- TheKeytoCryptography.py
+++ TheKeytoCryptography.py
@@ -1,16 +1,17 @@
 cipher = list(input())
 key = list(input())
+keyLength = len(key)
 msg = list()
 pos = 0
 
-while(len(key)-3!=len(cipher)):
-    mesage = ord(cipher[pos])-ord(key[pos])
-    if(mesage<0):
-        mesage+=26
-    if(mesage>25):
-        mesage-=26
-    msg.append(chr(mesage+65))
-    key.append(chr(mesage+65))
+while(len(key)-keyLength!=len(cipher)):
+    message = ord(cipher[pos])-ord(key[pos])
+    if(message<0):
+        message+=26
+    if(message>25):
+        message-=26
+    msg.append(chr(message+65))
+    key.append(chr(message+65))
     pos+=1
 
 print(''.join(msg))
