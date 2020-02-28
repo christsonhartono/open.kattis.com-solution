965951f5be9ab44b039e1ec8102d6ae3748cc433
christson <jhanacakro1@gmail.com>
2020-02-28 11:49:19 +0700

    'Python'

---
+++ Östgötska.py
@@ -0,0 +1,11 @@
+word = input().split()
+
+countA = sum([1 for i in word
+              if 'ae' in i])
+
+wordLength = len(word)
+
+if(countA/wordLength >= 0.4):
+    print("dae ae ju traeligt va")
+else:
+    print("haer talar vi rikssvenska")
